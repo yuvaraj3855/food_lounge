2a5774f06c696a97b89287170beb89c54f5eb60b
lalu <e.prasad@merai.co>
2025-11-14 19:43:52 +0530

    env fixes

--- frontend/src/pages/ApiCalls/axios.ts
+++ frontend/src/pages/ApiCalls/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://10.11.7.95:3000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
