a745447d94d26ce3ba675280bb17136ae0a1ce20
lalu <e.prasad@merai.co>
2025-11-14 16:44:35 +0530

    Frontend changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -31,6 +31,7 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
+    
       <RouterProvider router={router} />
     </StrictMode>,
   )
---
+++ frontend/src/pages/Doctor/doctor.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../Navbar/navbar"
+
+export default function Doctor() {
+  return (
+    <div>
+      <Navbar/>
+    <div className="min-h-screen p-10">
+      <h1 className="text-3xl font-bold mb-4">Doctor Page</h1>
+      <p className="text-lg">Welcome to the Doctor Page.</p>
+    </div>
+    </div>
+  );
+}
